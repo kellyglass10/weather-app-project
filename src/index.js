f8dc4833542ceb66ef8f1f535475268d27be14fb
Kelly Glass <73360394+kellyglass10@users.noreply.github.com>
2023-12-20 21:07:30 -0800

    inject-forecast-html-from-js

--- src/index.js
+++ src/index.js
@@ -52,7 +52,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="forecast-day">
+    <div class="forecast-date">${day}</div>
+    <div class="forecast-icon">🌤️</div>
+    <div class="forecast-temperatures">
+      <div class="forecast-temperature">
+        <strong>15°F</strong>
+      </div>
+      <div class="forecast-temperature">9°F</div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Baltimore");
+displayForecast();
